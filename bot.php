4697a162e61e42a304529c1fd59c9b79bc9386a8
sopon1 <kawasakikoong@hotmail.com>
2019-07-01 12:06:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,12 +1,4 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-	<?php
+<?php
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'o651NudRMzsU5jfijiPgTiFpo2pAslFXFKL7/c9bUmXp8TmsF7zOm3DQUsCH3ctE0JRODvO4NLFe5eUu6oj+XVbhLjkr3q8DQXLscXy+vLFXBpWFDY+Hg6Z1lMr6LBamBrkNl3RZQZ83H+EoofmRLwdB04t89/1O/w1cDnyilFU='; 
 $channelSecret = 'f8bfb388c9ea304291635116d0547425';
@@ -41,5 +33,3 @@ function send_reply_message($url, $post_header, $post_body)
     return $result;
 }
 ?>
-</body>
-</html>
